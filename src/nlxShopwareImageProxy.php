4ae668ed2bec0a4477e29904aed36f25d4d3323b
vladislav.sultanov <vladislav.sultanov@netlogix.de>
2025-04-14 09:16:21 +0200

    FEAT: Add plugin default structure
    RELATED: SHOPWARE-145

---
+++ src/nlxShopwareImageProxy.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Created by netlogix GmbH & Co. KG
+ *
+ * @copyright netlogix GmbH & Co. KG
+ */
+
+namespace nlxShopwareImageProxy;
+
+use Shopware\Core\Framework\Plugin;
+
+class nlxShopwareImageProxy extends Plugin
+{
+}
